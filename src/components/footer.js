f0a90f209f577afa75120821d2d91464c4741dd8
Chelsea Kent <chelsea.kent@gmail.com>
2019-02-05 22:39:07 -0800

    fix tou link

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby';
 export default () => {
   return (
     <footer className="footer" role="contentinfo">
-      <Link to="/tos">Tearms of Use</Link>
+      <Link to="/tou">Tearms of Use</Link>
       <Link to="/privacy">Privacy Statement</Link>
       <span>Vin Di Bona Productions | 2019</span>
     </footer>

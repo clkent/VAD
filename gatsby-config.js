0366c93db4b7940e52207f4505f6889279a380cd
Chelsea Kent <chelsea.kent@gmail.com>
2019-02-06 11:45:36 -0800

    dato test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 'use strict';
+require('dotenv').config;
 
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: `198eee2f918f547238770839fab2d8`,
+        apiToken: process.env.DATO_API_TOKEN,
         preview: false,
         disableLiveReload: false
       }

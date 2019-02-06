6aaa509911e43879a0cc63a7d88c5952c264d2bf
Chelsea Kent <chelsea.kent@gmail.com>
2019-02-06 14:55:43 -0800

    clear out travis

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     {
       resolve: `gatsby-source-datocms`,
       options: {
-        apiToken: `198eee2f918f547238770839fab2d8`, //process.env.DATO_API_TOKEN,
+        apiToken: process.env.DATO_API_TOKEN,
         preview: false,
         disableLiveReload: false
       }
